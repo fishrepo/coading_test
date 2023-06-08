d1b3a90d6950977635ceb0a770d91c1147590fca
seafish <seafish5252@gmail.com>
2023-06-08 22:20:02 +0900

    quiz15

--- question.py
+++ question.py
@@ -1,11 +1,44 @@
-n = int(input())
-
-num = [int(x) for x in str(n)]
-
-half = int(len(num)/2)
+from collections import deque
 
 
-if sum(num[:half]) == sum(num[half:]):
-    print("LUCKY")
-else:
-    print("READY")
+def bfs(graph, start, visited, length):
+
+    queue = deque([start])
+
+    visited[start] = True
+    length[start] = 0
+
+    while queue:
+        v = queue.popleft()
+
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+                length[i] = length[v]+1
+
+
+
+n, m, k, x = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+
+    index, num = map(int, input().split())
+    graph[index].append(num)
+
+visited = [False]*(n+1)
+length = [0]*(n+1)
+
+bfs(graph, x, visited, length)
+
+if not k in length:
+    print(-1)
+
+for i in range(len(length)):
+    if length[i] == k:
+        print(i)
+        
+
+
