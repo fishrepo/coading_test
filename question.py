583e9129e6c2042c58d9bf94499f3702f7ccb2a7
seafish <seafish5252@gmail.com>
2023-06-08 19:01:10 +0900

    quiz07

--- question.py
+++ question.py
@@ -1,58 +1,11 @@
-import time
+n = int(input())
 
-int(input())
-fear = list(map(int, input().split()))
+num = [int(x) for x in str(n)]
 
-start_time = time.time()
-
-# print(fear)
-
-fear.sort()
-group = 0
-
-while len(fear) != 0:
-
-    max = fear[-1]
-
-    for i in range(max):
-        if len(fear) == 0:
-            break
-        fear.pop()
-
-    group += 1
-    # end_time = time.time()
-    # print("time :", end_time - start_time)
+half = int(len(num)/2)
 
 
-print(group)
-end_time = time.time()
-print("time :", end_time - start_time)
-
-
-# n = int(input())
-# data = list(map(int, input().split()))
-
-# start_time = time.time()
-
-# data.sort()
-
-# result = 0
-# count = 0
-
-# for i in data:
-#     count += 1
-#     if count >= i:
-#         result += 1
-#         count = 0
-
-# print(result)
-# end_time = time.time()
-# print("time :", end_time - start_time)
-
-# start_time = time.time()
-# for i in range(10):
-#     for j in range(10):
-#         print(j)
-        
-# end_time = time.time()
-# print("time :", end_time - start_time)
+if sum(num[:half]) == sum(num[half:]):
+    print("LUCKY")
+else:
+    print("READY")
