1bbc4e3aefead27e3dbf4d0093b71b34bebf1137
seafish <seafish5252@gmail.com>
2023-06-23 14:07:09 +0900

    quiz31

--- question.py
+++ question.py
@@ -1,16 +1,39 @@
-from bisect import bisect_left, bisect_right
-
-def count_by_range(array, left_value, right_value):
-    right_index = bisect_right(array, right_value)
-    left_index=  bisect_left(array, left_value)
-    return right_index - left_index
-
-n, x = map(int, input().split())
+T = input()
+n, m = map(int, input().split())
 array = list(map(int, input().split()))
 
-count = count_by_range(array, x, x)
+gold = []
+start = 0
+for i in range(n):
 
-if count == 0:
-    print(-1)
-else:
-    print(count)
+    gold.append(array[start:m*(i+1)])
+    start += m
+
+del_max = 0
+row = None
+column: None
+
+for i in range(n):
+    if del_max < gold[i][0]:
+        del_max = gold[i][0]
+        row = i
+
+del_sum = 0
+
+
+for j in range(m):
+
+    if row == 0:
+        del_value = max(gold[row][j], gold[row+1][j])
+        del_sum += del_value
+        row, column = gold.index(del_value)
+
+    elif row == n:
+        del_value = max(gold[row][j], gold[row-1][j])
+        del_sum += del_value
+        row, column = gold.index(del_value)
+
+    else:
+        del_value = max(gold[row][j], gold[row+1][j], gold[row-1][j])
+        del_sum += del_value
+        row, column = gold.index(del_value)
