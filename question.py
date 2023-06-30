c2357cf28b0d74992675d236de85850cf4a23acd
seafish <seafish5252@gmail.com>
2023-06-30 19:36:31 +0900

    quiz24

--- question.py
+++ question.py
@@ -1,59 +1,30 @@
-n, m = map(int, input().split())
-array = []
-temp = [[0]*m for _ in range(n)]
+# INF = int(1e9)
 
-for i in range(m):
-    array.append(list(map(int, input().split())))
+# N = int(input())
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+# house = list(map(int, input().split()))
 
-def virus(x, y):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+# min_num = min(house)
+# max_num = max(house)
+
+
+# min_sum = INF
+# point = 0
+# for i in range(min_num,max_num+1):
+#     length = 0
+#     for j in house:
+#         length += abs(i-j)
+#     if length < min_sum:
+#         min_sum = length
+#         point = i
         
-        if nx >= 0 and nx < n and ny >= 0 and ny < m:
-            if temp[nx][ny] == 0:
-                temp[nx][ny] = 2
-                virus(nx, ny)
+# print(point)
 
-def get_score():
-    score = 0
-    for i in range(n):
-        for j in range(m):
-            if temp[i][j] == 0:
-                score +=1
-                
-    return score
-                
-def dfs(count):
-    global result
-    
-    if count == 3:
-        for i in range(n):
-            for j in range(m):
-                temp[i][j] = array[i][j]
-                
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 2:
-                    virus(i,j)
-                    
-        result = max(result, get_score())
-        return
-    
-    for i in range(n):
-        for j in range(m):
-            if array[i][j] == 0:
-                array[i][j] = 1
-                count += 1
-                dfs(count)
-                array[i][j] = 0
-                count -= 1
-                
-dfs(0)
-print(result)
-                
-        
+
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+print(3/2)
+print(data[(n-1)//2])
     
