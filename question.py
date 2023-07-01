c75960c45f3a336287b5e1a73f1db0f8ee30e89e
seafish <seafish5252@gmail.com>
2023-07-01 17:28:04 +0900

    quiz38

--- question.py
+++ question.py
@@ -1,20 +1,31 @@
-n = int(input())
-tower = []
-for i in range(n):
-    tower.append(list(map(int, input().split())))
+INF = int(1e9)
 
-temp = [[]]
+n, m = map(int, input().split())
 
-for i in range(n-1):
-    temp.append(tower[i])
+graph = [[INF]*(n+1) for _ in range(n+1)]
 
+for a in range(1, n+1):
+    for b in range(1,n+1):
+        if a == b:
+            graph[a][b] = 0
+            
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+            
+result = 0
 
-for i in range(n-1, 0, -1):
-    for j in range(len(tower[i])-1):
-        if tower[i][j] > tower[i][j+1]:
-            temp[i][j] = tower[i][j]+temp[i][j]
-        else:
-            temp[i][j] = tower[i][j+1]+temp[i][j]
+for i in range(1 , n+1):
+    count = 0
+    for j in range(1, n+1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1
+        if count == n:
+            result +=1
 
-
-print(*temp[1])
+print(result)
