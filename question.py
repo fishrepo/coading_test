cd0fb2d5d5df824ccf266a22226a5b0d903d59fd
seafish <seafish5252@gmail.com>
2023-07-02 19:17:55 +0900

    quiz42

--- question.py
+++ question.py
@@ -1,31 +1,32 @@
-INF = int(1e9)
+G = int(input())
+P = int(input())
 
-n, m = map(int, input().split())
+gi = []
+check = [0 for i in range(G+1)]
 
-graph = [[INF]*(n+1) for _ in range(n+1)]
 
-for a in range(1, n+1):
-    for b in range(1,n+1):
-        if a == b:
-            graph[a][b] = 0
-            
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
+
+for i in range(P):
+    gi.append(int(input()))
     
-for k in range(1, n+1):
-    for a in range(1, n+1):
-        for b in range(1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+
+print(check)   
+for i in gi:
+    out = 0
+    for j in range(i,0,-1):
+        print(j)
+        if check[j] == 0:
+            check[j] = 1
+            break
+        
+        if j == 1 and check[j] != 0:
+            out = 1
             
-result = 0
+    if out == 1 :
+        break
+    
+result = check.count(1)
 
-for i in range(1 , n+1):
-    count = 0
-    for j in range(1, n+1):
-        if graph[i][j] != INF or graph[j][i] != INF:
-            count += 1
-        if count == n:
-            result +=1
+print(result)  
 
-print(result)
