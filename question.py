ccf78d8ca184cf63ef4d9fedeec3b107872c7834
seafish <seafish5252@gmail.com>
2023-07-03 19:47:28 +0900

    quiz03

--- question.py
+++ question.py
@@ -1,32 +1,18 @@
-G = int(input())
-P = int(input())
+S= input()
 
-gi = []
-check = [0 for i in range(G+1)]
+number = [int(i) for i in S]
 
+count0 = 0
+count1 = 0
 
+for i in range(len(S)-1):
+  if number[i] != number[i+1]:
+    if number[i] == 0:
+      count0 += 1
+    else:
+      count1 += 1
 
-for i in range(P):
-    gi.append(int(input()))
-    
+result = min(count0,count1)
 
-
-print(check)   
-for i in gi:
-    out = 0
-    for j in range(i,0,-1):
-        print(j)
-        if check[j] == 0:
-            check[j] = 1
-            break
-        
-        if j == 1 and check[j] != 0:
-            out = 1
-            
-    if out == 1 :
-        break
-    
-result = check.count(1)
-
-print(result)  
+print(result)
 
