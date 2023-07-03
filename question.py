702bb227e561d5efe11892a3738f3be54f5344c4
seafish <seafish5252@gmail.com>
2023-07-03 21:14:06 +0900

    quiz 29

--- question.py
+++ question.py
@@ -1,36 +1,34 @@
-from collections import deque
+N, C = map(int, input().split())
 
-n, k = map(int, input().split())
+x = []
+for i in range(N):
+    x.append(int(input()))
+    
+x.sort()
 
-graph = []
-data = []
+start = 1
+end = x[-1] - x[0]
+result = 0
 
-for i in range(n):
-  graph.append(list(map(int, input().split())))
-  for j in range(n):
-    if graph[i][j] != 0:
-      data.append((graph[i][j], 0, i, j))
-
-data.sort()
-q = deque(data)
-
-target_s,target_x, target_y = map(int, input().split())
-
-dx = [-1, 0, 1, 0]
-dy = [0,1,0,-1]
-
-while q:
-  virus, s, x, y = q.popleft()
-  if s == target_s:
-    break
-  for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
-
-    if 0 <= nx and nx < n and 0 <= ny and ny < n:
-      if graph[nx][ny] == 0:
-        graph[nx][ny] = virus
-        q.append((virus, s+1, nx, ny))
-
-print(graph[target_x-1][target_y-1])
-
+while start <= end :
+    mid = (start+end)//2
+    value = x[0]
+    count = 1
+    
+    for i in range(1,N):
+        if x[i] >= value+mid:
+            value = x[i]
+            count += 1
+    
+    if count >= C:
+        start = mid +1
+        result = mid
+    else:
+        end = mid -1
+        
+print(result)
+        
+            
+        
+    
+    
