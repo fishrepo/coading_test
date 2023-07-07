3756e7c60183cd1abfed07571f7ddc1a84c25f8f
seafish <seafish5252@gmail.com>
2023-07-07 20:19:39 +0900

    quiz 04

--- question.py
+++ question.py
@@ -1,49 +1,13 @@
-def rotate_a_matrix_by_90_degree(a):
-  n = len(a)
-  m = len(a[0])
-  result = [[0]*n for _ in range(m)]
-  for i in range(n):
-    for j in range(m):
-      result[j][n-i-1] = a[i][j]
-  return result
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
 
-def check(new_lock):
-  lock_length = len(new_lock) // 3
-  for i in range(lock_length, lock_length * 2):
-    for j in range(lock_length,lock_length * 2):
-      if new_lock[i][j] != 1:
-        return False
-  return True
+target = 1
+for x in data:
+  
+  if target < x:
+    break
+  target += x
+  
+print(target)
 
-
-m, n = map(int, input().split())
-
-key = []
-lock = []
-
-for i in range(m):
-  key.append(list(map(int,input().split())))
-
-for i in range(n):
-  lock.append(list(map(int, input().split())))
-
-new_lock = [[0]*(n*3) for _ in range(n*3)]
-
-for i in range(n):
-  for j in range(n):
-    new_lock[i + n][j + n] = lock[i][j]
-
-for rotation in range(4):
-  key = rotate_a_matrix_by_90_degree(key)
-  for x in range(n*2):
-    for y in range(n*2):
-      for i in range(m):
-        for j in range(m):
-          new_lock[x+i][y+j] += key[i][j]
-      if check(new_lock)== True:
-          print(True)
-          exit(0)
-      for i in range(m):
-        for j in range(m):
-          new_lock[x+i][y+j] -= key[i][j]
-print(False)
