e63d7000e5e2e70f0ee64fb3447a0b4bb8bc1a00
seafish <seafish5252@gmail.com>
2023-07-08 22:47:35 +0900

    quiz 26

--- question.py
+++ question.py
@@ -1,13 +1,63 @@
-n = int(input())
-data = list(map(int, input().split()))
-data.sort()
+def Balance_s(w):
+    countL = 0
+    countR = 0
+    countL += w.count('(')
+    countR += w.count(')')
 
-target = 1
-for x in data:
-  
-  if target < x:
-    break
-  target += x
-  
-print(target)
+    return countL, countR
 
+
+def Right_s(s):
+    count = 0
+
+    for i in s:
+        if i == '(':
+            count += 1
+        elif i == ')':
+            count -= 1
+
+        if count < 0:
+            return False
+
+    return True
+
+
+def Separate(w):
+    count_L, count_R = Balance_s(w)
+    s = ''
+    if count_L == count_R:
+        for i in range(1, len(w)+1):
+            print(w[:i])
+            L, R = Balance_s(w[:i])
+            
+            if L == R:
+                u = w[:i]
+                v = w[i:]
+                if Right_s(u) == True:
+                    s += w[:i]
+                    s += Separate(v)
+                    # break로 끊어주기
+                    break
+                else:
+                    s += '('
+                    s += Separate(v)
+                    s += ')'
+
+                    temp = u[1:len(u)-1]
+
+                    for i in temp:
+                        if i == '(':
+                            s += ')'
+                        else:
+                            s += '('
+                    # break로 끊어주기
+                    break
+            else:
+                continue
+
+    return s
+
+
+s = '()))((()'
+
+print(Separate(s))
