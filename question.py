148148e318021aa9c2fb4967e644a00929bcc3d1
seafish <seafish5252@gmail.com>
2023-07-14 21:15:19 +0900

    quiz12

--- question.py
+++ question.py
@@ -1,25 +1,51 @@
-food_times = list(map(int,input().split()))
-k = int(input())
+#a : 0은 기둥, 1은 보
+#b : 0은 삭제, 1은 설치
+n = int(input())
 
-result = 0
-index = 0
-while True:
+build_frame = [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
+#build_frame = [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]
+
+
+build = [[' ']*(n+2) for i in range(n+2)]
+
+re_frame = []
+
+for frame in build_frame:
   
-  print(k)
-  i = index % len(food_times)
-  if food_times[i] != 0:
-    food_times[i] -= 1
-    result = i
-    k -= 1
-    index += 1
-  elif food_times[i] == 0:
-    index += 1
-  print(food_times)
-  if k < 0:
-    break
+  x, y, a, b = frame
+  if b == 1:
+    if a == 0:
+      if y == 0 or build[y][x-1] == '-' or build[y][x+1] == '-' or build[y-1][x] == 'I':
+        build[y][x] = 'I'
+      else:
+        continue
+    else:
+      if build[y-1][x] == 'I' or build[y][x-1] == '-' or build[y][x+1] == '-':
+        build[y][x] = '-'
+      else:
+        continue
+      
+  else:
+    if a == 0:
+      if y == 0 or build[y][x-1] == '-' or build[y][x+1] == '-' or build[y-1][x] == 'I':
+        build[y][x] = ' '
+      else:
+        continue
+    else:
+      if build[y][x-1] == '-' and build[y][x+1] == '-':
+        build[y][x] = '-'
+      else:
+        build[y][x] = ' '
 
-  if len(set(food_times)) <= 1:
-    break
 
+      
+  
+    
+for i in range(n+1):
+  for j in range(n+1):
+    if build[i][j] != ' ':
+      re_frame.append([j,i,build[i][j]])
 
-print(result)
+re_frame.sort()
+print(build)
+print(re_frame)
