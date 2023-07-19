60a7c135f96cc31be52af2c91929e837b30092b1
seafish <seafish5252@gmail.com>
2023-07-19 18:45:05 +0900

    quiz36

--- quiz.py
+++ quiz.py
@@ -1,67 +1,25 @@
-from collections import deque
+def edit_dist(str1, str2):
+    n = len(str1)
+    m = len(str2)
+
+    dp = [[0] * (m+1) for _ in range(n+1)]
+
+    for i in range(1, n + 1):
+        dp[i][0] = i
+    for j in range(1, m + 1):
+        dp[0][j] = j
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1])
+
+    return dp[n][m]
 
 
-n = int(input())
+str1 = input()
+str2 = input()
 
-indegree = [0] * (n+1)
-
-graph = [[False] * (n + 1) for i in range(n+1)]
-
-data = list(map(int, input().split()))
-
-for i in range(n):
-    for j in range(i+1, n):
-        graph[data[i]][data[j]] = True
-        indegree[data[j]] += 1
-
-m = int(input())
-
-for i in range(m):
-    a, b = map(int, input().split())
-
-    if graph[a][b]:
-        graph[a][b] = False
-        graph[b][a] = True
-        indegree[a] += 1
-        indegree[b] -= 1
-    else:
-        graph[a][b] = True
-        graph[b][a] = False
-        indegree[a] -= 1
-        indegree[b] += 1
-
-result = []
-q = deque()
-
-for i in range(1, n+1):
-    if indegree[i] == 0:
-        q.append(i)
-
-certain = True
-cycle = False
-
-for i in range(n):
-    if len(q) == 0:
-        cycle = True
-        break
-    if len(q) >= 2:
-        certain = False
-        break
-    now = q.popleft()
-    result.append(now)
-
-    for j in range(1, n+1):
-        if graph[now][j]:
-            indegree[j] -= 1
-
-            if indegree[j] == 0:
-                q.append(j)
-
-if cycle:
-    print("INPOSSIBLE")
-elif not certain:
-    print("?")
-else:
-    for i in result:
-        print(i, end=' ')
-    print()
+print(edit_dist(str1, str2))
