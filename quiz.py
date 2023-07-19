87f7ceb6398a8b5636f0e6e12a47f9fca6c1bdb4
seafish <seafish5252@gmail.com>
2023-07-19 23:56:40 +0900

    quiz 20

--- quiz.py
+++ quiz.py
@@ -1,25 +1,94 @@
-def edit_dist(str1, str2):
-    n = len(str1)
-    m = len(str2)
+n = int(input())
 
-    dp = [[0] * (m+1) for _ in range(n+1)]
-
-    for i in range(1, n + 1):
-        dp[i][0] = i
-    for j in range(1, m + 1):
-        dp[0][j] = j
-
-    for i in range(1, n+1):
-        for j in range(1, m+1):
-            if str1[i - 1] == str2[j - 1]:
-                dp[i][j] = dp[i-1][j-1]
-            else:
-                dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1])
-
-    return dp[n][m]
+array = []
+temp = [[0] * n for _ in range(n)]
 
 
-str1 = input()
-str2 = input()
+for _ in range(n):
+    array.append(list(input().split()))
 
-print(edit_dist(str1, str2))
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+student = 3
+result = 'no'
+
+
+def go_straight(i, x, y):
+    global student
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if nx >= 0 and nx < n and ny >= 0 and ny < n:
+        if temp[nx][ny] == 'X':
+            temp[nx][ny] = 'V'
+            go_straight(i, nx, ny)
+
+        # if temp[nx][ny] == 'O':
+
+        if temp[nx][ny] == 'S':
+            student -= 1
+            temp[nx][ny] = 'V'
+            go_straight(i, nx, ny)
+
+
+def view(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx >= 0 and nx < n and ny >= 0 and ny < n:
+            if array[nx][ny] == 'X' or array[nx][ny] =='S':
+                temp[nx][ny] = 'V'
+                go_straight(i, nx, ny)
+
+
+debug = 0
+
+
+def dfs(count):
+    global student
+    global result
+    global debug
+
+    if count == 3:
+        for i in range(n):
+            for j in range(n):
+                temp[i][j] = array[i][j]
+
+        for i in range(n):
+            for j in range(n):
+                if temp[i][j] == 'T':
+                    view(i, j)
+                    
+        if debug == 1:
+            print()
+            print()
+            
+            
+        print(student)
+        if student == 3:
+            print('yes')
+            result = 'yes'
+            
+        else:
+            student = 3
+        #   print('no')
+            
+        return
+    for i in range(n):
+
+        for j in range(n):
+            if array[i][j] == 'X':
+                array[i][j] = 'O'
+                count += 1
+                
+                if count == 1 and i == 0 and j == 3:
+                    debug = 1
+
+                dfs(count)
+
+                array[i][j] = 'X'
+                count -= 1
+
+
+dfs(0)
+print(result)
