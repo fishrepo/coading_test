5fca4af630842b2752ad125cb059ed6622cbedf4
seafish <seafish5252@gmail.com>
2023-07-24 22:58:41 +0900

    quiz13

--- quiz.py
+++ quiz.py
@@ -1,60 +1,45 @@
-from collections import deque
+from itertools import combinations
 
-n, l, r = map(int, input().split())
+def find_length(r1,c1,field,n):
+  min_length = 1e9
+  for r2 in range(n):
+    for c2 in range(n):
+      if field[r2][c2] == 2:
+        length = abs(r1 - r2) + abs(c1 - c2)
+        min_length = min(min_length,length)
+  return min_length
 
-graph = []
+n, m = map(int, input().split())
+
+field = []
+
 for _ in range(n):
-  graph.append(list(map(int, input().split())))
+  field.append(list(map(int, input().split())))
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
 
-result = 0
+chicken = []
 
-def process(x, y, index):
-  united = []
-  united.append((x,y))
+for i in range(n):
+  for j in range(n):
+    if field[i][j] == 2:
+      chicken.append((i,j))
 
-  q = deque()
-  q.append((x,y))
-  
-  union[x][y] = index
-  summary = graph[x][y]
-  count = 1
 
-  while q:
-    x, y = q.popleft()
+selects = list(combinations(chicken, len(chicken)-m))      
 
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
+min_length = 1e9
 
-      if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
-        if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
-          q.append((nx,ny))
-          union[nx][ny] = index
-          summary += graph[nx][ny]
-          count += 1
-          united.append((nx,ny))
-
-  for i,j in united:
-    graph[i][j] = summary//count
-  return count
-       
-    
-
-total_count = 0
-
-while True:
-  union = [[-1] * n for _ in range(n)]
-  index = 0
+for select in selects:
+  print(select)
+  for x, y in select:
+    field[x][y] = 0
+  sum_length = 0
+      
   for i in range(n):
     for j in range(n):
-      if union[i][j] == -1:
-        process(i, j, index)
-        index += 1
-  if index == n*n:
-    break
-  total_count += 1
-
-print(total_count)
+      if field[i][j] == 1:
+        sum_length += find_length(i,j,field,n)
+  min_length = min(sum_length,min_length)
+  for x, y in select:
+    field[x][y] = 2
+print(min_length)  
