73e695209f5958f7173a9f2a65b0906122713ac3
seafish <seafish5252@gmail.com>
2023-08-14 21:00:56 +0900

    quiz15~quiz21

--- quiz.py
+++ quiz.py
@@ -1,273 +1,517 @@
-
 ```python
-n = int(input())
+from collections import deque
 
-num = [int(x) for x in str(n)]
+def bfs(graph, start, visited, length):
+    queue = deque([start])
+    visited[start] = True
+    length[start] = 0
 
-half = int(len(num)/2)
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+                length[i] = length[v]+1
 
-if sum(num[:half]) == sum(num[half:]):
-    print("LUCKY")
-else:
-    print("READY")
-```
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-```python
-string = input()
+        if nx >= 0 and nx < n and ny >= 0 and ny < m:
+            if temp[nx][ny] == 0:
+                temp[nx][ny] = 2
+	              virus(nx, ny)
 
-number = [int(i) for i in string if ord(i)<58 ]
-
-abc = [i for i in string if ord(i)>58]
-
-abc.sort()
-number = sum(number)
-
-for i in abc:
-  print(i,end='')
-print(number)
-```
-
-```python
-s = input()
-
-answer = len(s)
-
-for i in range(1,len(s)):
-    str_temp = s[0:i]
-    count = 1
-    compressed = ''
-    for j in range(i, len(s), i):
-        # if j+i >= len(s):
-        #     compressed += str(count) + str_temp if count >= 2 else str_temp
-        #     break
-        
-        if str_temp == s[j:j+i]:
-            
-            count += 1
-
-        else: 
-            compressed += str(count) + str_temp if count >= 2 else str_temp
-            str_temp = s[j:j+i]
-            count = 1
-				
-        if j+i >= len(s):
-            compressed += str(count) + str_temp if count >= 2 else str_temp
-            break
-
-    # print(compressed)        
-    answer = min(answer,len(compressed))
-
-print(answer)
-```
-
-```python
-def rotate_a_matrix_by_90_degree(a):
-  n = len(a)
-  m = len(a[0])
-  result = [[0]*n for _ in range(m)]
-  for i in range(n):
+for i in range(n):
     for j in range(m):
-      result[j][n-i-1] = a[i][j]
-  return result
+        if array[i][j] == 0:
+            array[i][j] = 1
+            count += 1
+            dfs(count)
+            array[i][j] = 0
+            count -= 1
 
-def check(new_lock):
-  lock_length = len(new_lock) // 3
-  for i in range(lock_length, lock_length * 2):
-    for j in range(lock_length,lock_length * 2):
-      if new_lock[i][j] != 1:
-        return False
-  return True
+while q:
+  virus, s, x, y = q.popleft()
+  if s == target_s:
+    break
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
 
-m, n = map(int, input().split())
-
-key = []
-lock = []
-
-for i in range(m):
-  key.append(list(map(int,input().split())))
-
-for i in range(n):
-  lock.append(list(map(int, input().split())))
-
-new_lock = [[0]*(n*3) for _ in range(n*3)]
-
-for i in range(n):
-  for j in range(n):
-    new_lock[i + n][j + n] = lock[i][j]
-
-for rotation in range(4):
-  key = rotate_a_matrix_by_90_degree(key)
-  for x in range(n*2):
-    for y in range(n*2):
-      for i in range(m):
-        for j in range(m):
-          new_lock[x+i][y+j] += key[i][j]
-      if check(new_lock)== True:
-          print(True)
-          exit(0)
-      for i in range(m):
-        for j in range(m):
-          new_lock[x+i][y+j] -= key[i][j]
-print(False)
+    if 0 <= nx and nx < n and 0 <= ny and ny < n:
+      if graph[nx][ny] == 0:
+        graph[nx][ny] = virus
+        q.append((virus, s+1, nx, ny))
 ```
 
 ```python
 from collections import deque
 
-n = int(input())
-k = int(input())
-apples = []
-board = [[0] * n for i in range(n)]
+def bfs(graph, start, visited, length):
+    queue = deque([start])
+    visited[start] = True
+    length[start] = 0
 
-for i in range(k):
-  x, y = map(int, input().split())
-  apples.append((x-1, y-1))
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+                length[i] = length[v]+1
 
-for apple in apples:
-  x, y = apple
-  board[x][y] = 2
+n, m, k, x = map(int, input().split())
 
-L = int(input())
+graph = [[] for _ in range(n+1)]
 
-X_C = []
+for _ in range(m):
 
-east = (0,1)
-west = (0,-1)
-south = (1,0)
-north = (-1,0)
+    index, num = map(int, input().split())
+    graph[index].append(num)
 
-direct_xy = [south,west,north]
-direct_xy = deque(direct_xy)
+visited = [False]*(n+1)
+length = [1e9]*(n+1)
 
-for i in range(L):
-  X, C = input().split()
-  X = int(X)
-  X_C.append((X, C))
-X_C = deque(X_C)
+bfs(graph, x, visited, length)
 
-time = 0
-board[0][0] = 1
-direct = east
-x, y = 0, 0
+if not k in length:
+    print(-1)
 
-loop = True
-stop = 0
-snake = [(0,0)]
-snake = deque(snake)
-while loop == True:
+for i in range(len(length)):
+    if length[i] == k:
+        print(i)
+====================================================================
 
-  if X_C:
-    X, C = X_C.popleft()  
+from collections import deque
 
-  while True:
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
 
-    time += 1
-    next_x = x + direct[0]
-    next_y = y + direct[1]
+for _ in range(m):
+  a, b = map(int, input().split())
+  graph[a].append(b)
 
-    if next_x < 0 or next_y < 0 or next_x >= n or next_y >= n:
-      loop = False
-      break
+distance = [-1] * (n+1)
+distance[x] = 0
 
-    if board[next_x][next_y] == 0:
+q = deque([x])
+while q:
+  now = q.popleft()
 
-      board[next_x][next_y] = 1
-      a,b = snake.popleft()
-      board[a][b] = 0
-      snake.append((next_x,next_y))
-      x = next_x
-      y = next_y
+for next_node in graph[now]:
+  if distance[next_node] == -1:
+    distance[next_node] = distance[now] + 1
+    q.append(next_node)
 
-    elif board[next_x][next_y] == 2:
-      board[next_x][next_y] = 1
-      snake.append((next_x,next_y))
-      x = next_x
-      y = next_y
+check = False
+for i in range(1, n+1):
+  if distance[i] == k:
+    print(i)
+    check = True
 
-    elif board[next_x][next_y] == 1:
-      loop = False
-      break
-
-    if time == X:
-      if C == 'D':
-        direct_xy.append(direct)
-        direct = direct_xy.popleft()
-        break
-      if C == 'L':
-        direct_xy.appendleft(direct)
-        direct = direct_xy.pop()
-        break
-
-print(time)
+if check == False:
+  print(-1)
 ```
 
 ```python
-def possible(answer):
-    for x, y, stuff in answer:
-        if stuff == 0:
-            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
-                continue
-            else:
-                return False
-        elif stuff == 1:
-            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer):
-                continue
-            else:
-                return False
+n, m = map(int, input().split())
+array = []
+temp = [[0]*m for _ in range(n)]
+
+for i in range(m):
+    array.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if nx >= 0 and nx < n and ny >= 0 and ny < m:
+            if temp[nx][ny] == 0:
+                temp[nx][ny] = 2
+                virus(nx, ny)
+
+def get_score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score +=1
+
+    return score
+
+def dfs(count):
+    global result
+
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = array[i][j]
+
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i,j)
+
+        result = max(result, get_score())
+        return
+
+    for i in range(n):
+        for j in range(m):
+            if array[i][j] == 0:
+                array[i][j] = 1
+                count += 1
+                dfs(count)
+                array[i][j] = 0
+                count -= 1
+
+dfs(0)
+print(result)
+```
+
+```python
+from collections import deque
+
+n, k = map(int, input().split())
+
+graph = []
+data = []
+
+for i in range(n):
+  graph.append(list(map(int, input().split())))
+  for j in range(n):
+    if graph[i][j] != 0:
+      data.append((graph[i][j], 0, i, j))
+
+data.sort()
+q = deque(data)
+
+target_s,target_x, target_y = map(int, input().split())
+
+dx = [-1, 0, 1, 0]
+dy = [0,1,0,-1]
+
+while q:
+  virus, s, x, y = q.popleft()
+  if s == target_s:
+    break
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+
+    if 0 <= nx and nx < n and 0 <= ny and ny < n:
+      if graph[nx][ny] == 0:
+        graph[nx][ny] = virus
+        q.append((virus, s+1, nx, ny))
+
+print(graph[target_x-1][target_y-1])
+```
+
+```python
+def Balance_s(w):
+    countL = 0
+    countR = 0
+    countL += w.count('(')
+    countR += w.count(')')
+
+    return countL, countR
+
+def Right_s(s):
+    count = 0
+
+    for i in s:
+        if i == '(':
+            count += 1
+        elif i == ')':
+            count -= 1
+
+        if count < 0:
+            return False
+
     return True
 
-build_frame = [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
-# build_frame = [[0, 0, 0, 1], [2, 0, 0, 1], [4, 0, 0, 1], [0, 1, 1, 1], [
-#     1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1, 1], [2, 0, 0, 0], [1, 1, 1, 0], [2, 2, 0, 1]]
-n = 5
-answer = []
+def Separate(w):
+    count_L, count_R = Balance_s(w)
+    
+    s = ''
+    if count_L == count_R:#문자열 w 가 균형잡힌 괄호 문자열이라면 실행
+        for i in range(1, len(w)+1):
+            print(w[:i])
+            L, R = Balance_s(w[:i])
+            #문자열 w를 두 '균형잡힌 괄호 문자열' u,v로 분리합니다. 단,u는'균형잡인 괄호 문자열'
+            #로 더 이상 분리할 수 없어야 하며,v는 빈 문자열이 될 수 있습니다.
+            if L == R:
+                u = w[:i]
+                v = w[i:]
+                if Right_s(u) == True:
+                  #문자열 u가 '올바른 괄호 문자열'이라면 문자열 v에 대해
+                  #1단계부터 다시 수행합니다.
+                    s += w[:i]
+                    s += Separate(v)
+                    # 더 이상 분리할수 없어야 하는 것은 for 문에서 최소값이므로 break로 끊어주기
+                    break
+                else:
+                  #문자열 u가'올바른 괄호 문자열'이 아니라면 아래 과정을 수행합니다.
+                  #빈 문자열에 첫번째 문자로 '('를 붙입니다.
+                  #문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다.
+                  #')'다시 붙입니다.
+                  #u의 첫번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
+                  #생성관 문자열을 반환합니다.
+                    s += '('
+                    s += Separate(v)
+                    s += ')'
 
-for frame in build_frame:
-    x, y,  stuff, operate = frame
-    if operate == 0:
-        answer.remove([x, y, stuff])
-        if not possible(answer):
-            answer.append([x, y, stuff])
-    if operate == 1:
-        answer.append([x, y, stuff])
-        if not possible(answer):
-            answer.remove([x, y, stuff])
+                    temp = u[1:len(u)-1]
 
-answer.sort()
+                    for i in temp:
+                        if i == '(':
+                            s += ')'
+                        else:
+                            s += '('
+                    # 더 이상 분리할수 없어야 하는 것은 for 문에서 최소값이므로 break로 끊어주기
+                    break
+            else:
+                continue
 
-print(answer)
+    return s
+
+s = '()))((()'
+
+print(Separate(s))
 ```
 
 ```python
-from itertools import combinations
+n = int(input())
 
-n, m = map(int, input().split())
-chicken, house = [], []
+data = list(map(int, input().split()))
 
-for r in range(n):
-  data = list(map(int,input().split()))
-  for c in range(n):
-    if data[c] == 1:
-      house.append((r,c))
-    elif data[c] == 2:
-      chicken.append((r,c))
-      
-candidates = list(combinations(chicken,m))
+add, sub, mul, div = map(int,input().split())
 
-def get_sum(candidate):
-  result = 0
-  
-  for hx, hy in house:
-    temp = 1e9
-    for cx, cy in candidate:
-      temp = min(temp, abs(hx - cx) + abs(hy - cy))
-    
-    result += temp
-  return result
+min_value = 1e9
+max_value = -1e9
 
-result = 1e9
-for candidate in candidates:
-  result = min(result, get_sum(candidate))
-  
+def dfs(i, now):
+  global min_value, max_value, add, sub, mul, div
+
+  if i == n:#숫자 사이에 모두 연산자를 끼워 넣었을 때,
+    min_value = min(min_value, now)
+    max_value = max(max_value, now)
+    print('index', i)
+    print('min',min_value)
+    print('max',max_value)
+
+  else:
+    #n이 3이고,
+    #add, sub, mul, div 가 2, 0, 2, 0 일때, 2,0,0,0 부터 1,0,1,0 => 0,0,2,0 까지 순서대로 값을 확인한다.
+    if add > 0:
+      add -= 1
+      print('index', i)
+      print('add',add)
+      print('mul',mul)
+      dfs(i+1, now + data[i])
+      add += 1
+    if sub > 0:
+      sub -= 1
+      print('sub')
+      dfs(i+1, now - data[i])
+      sub +=1
+    if mul > 0:
+      mul -= 1
+      print('index', i)
+      print('add',add)
+      print('mul',mul)
+      dfs(i + 1, now * data[i])
+      mul += 1
+    if div >0:
+      div -= 1
+      print('div')
+      dfs(i + 1, int(now / data[i]))
+      div += 1
+
+dfs(1, data[0])
+
+print(max_value)
+print(min_value)
+```
+
+```python
+n = 5
+
+array = [['X','S','X','X','T'],
+        ['T','X','S','X','X'],
+        ['X','X','X','X','X'],
+        ['X','T','X','X','X'],
+        ['X','X','T','X','X']]
+temp = [[0] * n for _ in range(n)]
+
+#for _ in range(n):
+    #array.append(list(input().split()))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+student = 3
+result = 'no'
+
+def go_straight(i, x, y):
+    global student
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if nx >= 0 and nx < n and ny >= 0 and ny < n:
+        if temp[nx][ny] == 'X':
+            temp[nx][ny] = 'V'
+            go_straight(i, nx, ny)
+
+        if temp[nx][ny] == 'S':
+            student -= 1
+            temp[nx][ny] = 'V'
+            go_straight(i, nx, ny)
+
+def view(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx >= 0 and nx < n and ny >= 0 and ny < n:
+            if array[nx][ny] == 'X' or array[nx][ny] =='S':
+                temp[nx][ny] = 'V'
+                go_straight(i, nx, ny)
+debug_one = 0
+debug_two = 0
+debug_three = 0
+
+def dfs(count):
+    global student
+    global result
+    global debug_one
+    global debug_two
+    global debug_three
+
+    if count == 3:
+        for i in range(n):
+            for j in range(n):
+                temp[i][j] = array[i][j]
+
+        for i in range(n):
+            for j in range(n):
+                if temp[i][j] == 'T':
+                    view(i, j)
+
+        if student == 3:
+            result = 'yes'
+
+        else:
+            student = 3
+       
+
+        return
+    for i in range(n):
+        for j in range(n):
+
+                
+            if array[i][j] == 'X':
+                array[i][j] = 'O'
+                count += 1
+                
+                #재귀함수 디버깅
+                # if count == 1  and i == 0 and j == 3:
+                #     debug_one = 1
+                
+                # if debug_one == 1 and count == 2 and i == 1 and j == 1:
+                #     debug_two = 1
+                  
+                # if debug_two == 1 and count == 3 and i == 2 and j == 2:
+                #     debug_three = 1
+                  
+                # if debug_one == debug_two == debug_three == 1:
+                #     print()
+                #     print(array)
+                #     print(temp)
+                    
+
+                dfs(count)
+
+                array[i][j] = 'X'
+                count -= 1
+                
+
+dfs(0)
 print(result)
+```
+
+```python
+from collections import deque
+
+n, l, r = map(int, input().split())
+
+graph = []
+for _ in range(n):
+  graph.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+result = 0
+
+def process(x, y, index, union):
+  united = []
+  united.append((x,y))
+    #인구이동이 일어난 인덱스 리스트
+  q = deque()
+  q.append((x,y))
+    #조사할 인덱스 큐
+  union[x][y] = index
+    #조사한 위치를 표시
+  summary = graph[x][y]
+  count = 1
+
+  while q:
+      #조사를 해야할 인덱스 큐가 비어있을때 까지
+    x, y = q.popleft()
+
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+
+      if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
+          #범위를 벗어나지 않고 아직 처리가 되지 않았으면 
+        if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
+          q.append((nx,ny))
+          #인구 차이가 l,r 사이이면 q에 집어넣는다
+          union[nx][ny] = index
+          #조사한 위치를 표시 
+          summary += graph[nx][ny]
+          #인구이동 후 평균값을 계산하기위해 더해준다
+          count += 1
+          #인구이동이 일어난 나라를 증가시킨다
+          united.append((nx,ny))
+          #인구이동이 일어난 인덱스를 넣어준다
+
+  for i,j in united:
+    graph[i][j] = summary//count
+  return count
+
+total_count = 0
+
+while True:
+  union = [[-1] * n for _ in range(n)]
+  index = 0
+  for i in range(n):
+    for j in range(n):
+      if union[i][j] == -1:
+        #아직 인덱스를 조사하지 않았으면
+        process(i, j, index, union)
+        index += 1
+  if index == n*n:
+      #모든 인덱스를 조사하였을 때 인구이동이 없으면
+    break
+  total_count += 1
+  #모든 인덱스를 조사하였을 때 인구이동이 있으면
+
+print(total_count)
 ```
