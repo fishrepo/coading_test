26fbd6e8b678f5300c129c51675ab367dab15933
seafish <seafish5252@gmail.com>
2023-09-13 14:37:06 +0900

    quiz 41 ~45

--- quiz.py
+++ quiz.py
@@ -1,205 +1,280 @@
 ```python
-# 5
-# 14
-# 1 2 2
-# 1 3 3
-# 1 4 1
-# 1 5 10
-# 2 4 2
-# 3 4 1
-# 3 5 1
-# 4 5 3
-# 3 5 10
-# 3 1 8
-# 1 4 2
-# 5 1 7
-# 3 4 2
-# 5 2 4
-
-INF = int(1e9) #무한을 의미하는 값으로 10억을 설정
-
-#노드의 개수 및 간선의 개수를 입력받기
-n = int(input())
-m = int(input())
-# 2차원 리스트(그래프 표현,인덱스 1을 첫번째로 하기위해 n+1)를 만들고, 모든 값을 무한으로 초기화
-graph = [[INF]*(n + 1) for _ in range(n + 1)]
-
-for a in range(1, n+1):
-  for b in range(1, n+1):
-    #자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
-    if a == b:
-      graph[a][b] = 0
-
-#각 간선에 대한 정보를 입력받아, 그 값으로 초기화
-for _ in range(m):
-  #A에서 B로 가는 비용은 C라고 설정
-  a, b, c = map(int, input().split())
-  #가장 짧은 간선 정보만 저장
-  if c < graph[a][b]:
-    graph[a][b] = c
-
-#점화식에 따라 플로이드 워셜 알고리즘을 수행
-for k in range(1, n+1):
-  for a in range(1, n+1):
-    for b in range(1, n+1):
-      graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-
-#수행된 결과를 출력
-for a in range(1, n+1):
-  for b in range(1, n+1):
-    # 도달할 수 없는 경우 0 을 출력
-    if graph[a][b] == INF:
-      print(0, end=" ")
-      #도달할 수 있는 경우 거리를 출력
-    else:
-      print(graph[a][b], end=" ")
-  print()
+def find_parent(parent, 3):
+    if 2 != 3:
+        parent[x] = find_parent(parent, 2):
+                        if 2 != 1:
+                            parent[x] = find_parent(parent, 1):                                            
+                                            return 1
 ```
 
 ```python
-INF = int(1e9)# 무한을 의미하는 값으로 10억을 설정
+#특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+  #루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+  if parent[x] != x:
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
 
-#노드의 개수, 간선의 개수를 입력받기
+#두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
+  if a < b:
+    parent[b] = a
+  else:
+    parent[a] = b
+    
+#여행지의 개수와 여행 계획에 속한 여행지의 개수 입력받기
 n, m = map(int, input().split())
-# 2차원 리스트(그래프표현)를 만들고, 모든 값을 무한으로 초기화
-graph = [[INF]*(n+1) for _ in range(n+1)]
+parent = [0] * (n + 1)
 
-#자기자신에서 자기자신으로 가는 비용은 0으로 초기화
-for a in range(1, n+1):
-    for b in range(1,n+1):
-        if a == b:
-            graph[a][b] = 0
+#부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, n+1):
+  parent[i] = i
 
-#각 간선에 대한 정보를 입력받아, 그 값으로 초기화
-for _ in range(m):
-   #a에서 b로 가는 비용을 1로 설정
-    a, b = map(int, input().split())
-    graph[a][b] = 1
+#union 연산을 각각 수행
+for i in range(n):
+  data = list(map(int, input().split()))
+  for j in range(n):
+    if data[j] == 1: #연결된 경우 union 연산 수행
+      union_parent(parent, i+1, j+1)
+      
+#여행 계획 입력받기
+plan = list(map(int, input().split()))
 
-#점화식에 따라 플로이드 워셜 알고리즘을 수행
-for k in range(1, n+1):
-    for a in range(1, n+1):
-        for b in range(1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+result = True
+#여행 계획에 속하는 모든 노드의 루트가 동일한지 확인
+for i in range(m-1):
+  if find_parent(parent, plan[i]) != find_parent(parent, plan[i+1]):
+    result = False
+    
+#여행 계획에 속하는 모든 노드가 서로 연결되어 있는지(루트가 동일한지)확인
+if result:
+  print('yes')
+else:
+  print('no')
+```
 
-result = 0
-# 각 학생을 번호에 따라 한 명씩 확인하며 도달 가능한지 체크
-for i in range(1 , n+1):
-    count = 0
-    for j in range(1, n+1):
-        if graph[i][j] != INF or graph[j][i] != INF:
-          #i에서 j로 가거나 j에서 i로 가는 경로가 있으면 비교가 가능하므로 카운트해준다
-            count += 1
-        if count == n: #i노드가 모든노드와 비교가능하면 정확한 순위를 알 수 있다.
-            result +=1
+```python
+G = int(input())#탑승구의 수
+P = int(input())#비행기의 수
+
+check = [0 for i in range(G+1)]#(인덱스 1 번부터 시작하기 위해 +1)
+gi = []# 각 비행기가 도킹할 수 있는 탑승구의 정보(gi[i]가 3 이면 check[3] 부터 check[1])
+
+for i in range(P):
+    gi.append(int(input()))
+
+print(check)   
+for i in gi:
+    out = 0 # 공항 운행 중지 조건
+    for j in range(i,0,-1):#큰 탑승구의 번호부터 1번 탑승구까지 차례대로 탐색
+        print(j)
+        if check[j] == 0:#탑승구가 비어있으면
+            check[j] = 1#비행기 도킹
+            break #도킹했으면 다음 비행기 꺼내기
+
+        if j == 1 and check[j] != 0:# 1번 탑승구까지 모두 차있으면
+            out = 1 # 공항 운행 중지
+
+    if out == 1 :
+        break
+
+result = check.count(1)# 도킹한 비행기 수
 
 print(result)
 ```
 
 ```python
-import heapq
+#특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+  #루트 노드가 아니라면, 루트노드를 찾을 때까지 재귀적으로 호출
+  if parent[x] != x:
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
 
-INF = int(1e9)
+#두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
+  if a < b:
+    parent[b] = a
+  else:
+    parent[a] = b
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+#노드의 개수와 간선의 개수 입력받기
+n, m = map(int, input().split())
+parent = [0] * (n+1)# 부모 테이블 초기화
 
-#노드의 개수를 입력받기
-n = int(input())
-#전체 맵 정보를 입력받기
-graph = []
-for i in range(n):
-  graph.append(list(map(int, input().split())))
+#모든 간선을 담을 리스트
+edges = []
 
-#최단거리 테이블을 모두 무한을 ㅗ초기화
-distance = [[INF] * n for _ in range(n)]
+#부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1,n+1):
+  parent[i] = i
 
-x, y = 0, 0
-# 시작 노드로 가기 위한 비용은 (0,0) 위치의 값으로 설정하여, 큐에 삽입
-q = [(graph[x][y], x, y)]
-distance[x][y] = graph[x][y]
+#모든 간선에 대한 정보를 입력받기
+for _ in range(m):
+  x, y ,z = map(int, input().split()) #x와 y 사이에 z길이 만큼의 도로가 있다
+  # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+  edges.append((z, x, y))
 
-#다익스트라 알고리즘 수행
-while q:
-	#가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
-  dist, x, y = heapq.heappop(q)
-	#현재 노드가  이미 처리된 적이 있는 노드라면 무시
-  if distance[x][y] < dist:
-    continue
-	# 현재 노드와 연결된 다른 인접한 노드들을 확인
-  for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
-		#맵의 범위를 벗어나는 경우 무시
-    if nx < 0 or nx >= n or ny < 0 or ny >= n:
-      continue
-		
-    cost = dist + graph[nx][ny]
-		#현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-    if cost < distance[nx][ny]:
-      distance[nx][ny] = cost
-      heapq.heappush(q, (cost, nx, ny))
+#간선을 비용순으로 정렬
+edges.sort()
+total = 0 # 전체 가로등 비용
+result = 0 #간선을 줄인 최종 비용
+for edge in edges:
+  cost, a, b = edge
+  total += cost
+  
+  #사이클이 발생하지 않는 경우에만 집합에 포함
+  if find_parent(parent, a) != find_parent(parent, b):
+    union_parent(parent, a, b)
+    result += cost
 
-print(distance[n - 1][n - 1])
+print(total - result)
 ```
 
 ```python
-import heapq
+#특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+  #루트 노드가 아니라면, 루트노드를 찾을 때까지 재귀적으로 호출
+  if parent[x] != x:
+    parent[x] = find_parent(parent,parent[x])
+  return parent[x]
 
-INF = int(1e9)#무한을 의미하는 값으로 10억을 설정
+#두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
+  if a < b:
+    parent[b] = a
+  else:
+    parent[a] = b
 
-#노드의 개수, 간선의 개수를 입력받기
-n, m = map(int, input().split())
-#시작 노드를 1번 헛간으로 설정
-start = 1
-#각 노드에연결되어 있는 노드에 대한 정보를  담는 리스트를 만들기
-graph = [[] for i in range(n+1)]
-#최단 거리 테이블을 모두 무한으로 초기화
-distance = [INF] * (n+1)
-#모든 간선 정보를 입력받기
-for _ in range(m):
-  a,b = map(int, input().split())
-  #a번 노드와 b번 노드의 이동 비용이 1이라는 의미(양방형)
-  graph[a].append((b,1))
-  graph[b].append((a,1))
+#노드의 개수 입력받기
+n = int(input())
+parent = [0] * (n+1) #부모 테이블 초기화
 
-def dijkstra(start):
-  q = []
-  #시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
-  heapq.heappush(q, (0, start))
-  distance[start] = 0
-  while q:#큐가 비어있지 않다면
-    #가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
-    dist, now =  heapq.heappop(q)
-    #현재 노드가 이미 처리된 적이 있는 노드라면 무시(우선순위 큐이므로 무한이었던 거리가
-#현재 원소의 거리보다 짧아졌다는 의미는 거리가 더 짧은 경로가 나와가 이미 그 노드를 처리했다는 의미이다.) 
-    if distance[now] < dist:
-      continue
-    #현재 노드와 연결된 다른 인접한 노드들을 확인
-    for i in graph[now]:
-      cost = dist + i[1]
-      #현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-      if cost < distance[i[0]]:
-        distance[i[0]] = cost
-        heapq.heappush(q,(cost,i[0]))
+#모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = 0
 
-#다익스트라 알고리즘 수행
-dijkstra(start)
-
-#최단 거리가 가장 먼 노드 번호(동빈이가 숨을 헛간의 번호)
-max_node = 0
-#도달할 수 있는 노드 중에서, 최단 거리가 가장 먼 노드와의 최단거리
-max_distance = 0
-#최단 거리가 가장 먼 노드와의 최단 거리와 동일한 최단 거리를 가지는 노드들의 리스트
-result = []
-
+#부모 테이블상에서, 부모를 자기 자신으로 초기화
 for i in range(1, n+1):
-  if max_distance < distance[i]:
-    max_node = i
-    max_distance = distance[i]
-    result = [max_node]
-  elif max_distance == distance[i]:
-    result.append(i)
+  parent[i] = i
 
-print(max_node, max_distance, len(result))
+x = []
+y = []
+z = []
+
+#모든 노드에 대한 좌표 값 입력받기
+for i in range(1, n+1):
+  data = list(map(int, input().split()))
+  x.append((data[0], i))
+  y.append((data[1], i))
+  z.append((data[2], i))
+
+x.sort()
+y.sort()
+z.sort()
+
+#인접한 노드들로부터 간선 정보를 추출하여 처리
+for i in range(n - 1):
+  #비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+  edges.append((x[i+1][0] - x[i][0], x[i][1], x[i+1][1]))
+  edges.append((y[i+1][0] - y[i][0], y[i][1], y[i+1][1]))
+  edges.append((z[i+1][0] - z[i][0], z[i][1], z[i+1][1]))
+
+#간선을 비용순으로 정렬
+edges.sort()
+
+#간선을 하나씩 확인하며
+for edge in edges:
+  cost, a, b = edge
+  #사이클이 발생하지 않는 경우에만 집합에 포함
+  if find_parent(parent, a) != find_parent(parent, b):
+    union_parent(parent, a, b)
+    result += cost
+
+print(result)
+```
+
+```python
+from collections import deque
+
+# 노드의 개수 입력 받기
+n = int(input())
+# 모든 노드에 대한 진입차수는 0으로 초기화
+indegree = [0] * (n+1)
+# 각 노드에 연결돤 간선 정보를 담기 위한 인접 행렬 초기화
+graph = [[False] * (n + 1) for i in range(n+1)]
+# 작년 순위 정보 입력
+data = list(map(int, input().split()))
+# 방향 그래프의 간선 정보 초기화
+for i in range(n):
+    for j in range(i+1, n):
+        graph[data[i]][data[j]] = True
+        indegree[data[j]] += 1
+
+# 올해 변경된 순위 정보 입력
+m = int(input())
+
+for i in range(m):
+    a, b = map(int, input().split())
+    # 간선의 방향 뒤집기
+    if graph[a][b]:
+        graph[a][b] = False
+        graph[b][a] = True
+        indegree[a] += 1
+        indegree[b] -= 1
+    else:
+        graph[a][b] = True
+        graph[b][a] = False
+        indegree[a] -= 1
+        indegree[b] += 1
+
+# 위상정렬 시작
+result = []  # 알고리즘 수행 결과를 담을 리스트
+q = deque()  # 큐 기능을 위한 deque 라이브러리 사용
+
+# 처음 시작할 때는 진입차수가 0 인 노드를 큐에 삽입
+for i in range(1, n+1):
+    if indegree[i] == 0:
+        q.append(i)
+
+certain = True  # 위상 정렬 결과가 오직 하나인지의 여부
+cycle = False  # 그래프 내 사이클이 존재하는지 여부
+
+# 정황히 노드의 개수만큼 반복
+for i in range(n):
+    # 큐가 비어 있다면 사이클이 발생했다는 의미
+    if len(q) == 0:
+        cycle = True
+        break
+    # 큐의 원소가 2개 이상이라면 가능한 정렬결과가 여러 개라는 의미
+    if len(q) >= 2:
+        certain = False
+        break
+    # 큐에서 원소 꺼내기
+    now = q.popleft()
+    result.append(now)
+    # 해당 원소와 연결된 노드들의 진입차수에서 1빼기
+    for j in range(1, n+1):
+        if graph[now][j]:
+            indegree[j] -= 1
+            # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+            if indegree[j] == 0:
+                q.append(j)
+# 사이클이 발생하는 경우
+if cycle:
+    print("INPOSSIBLE")
+# 위상 정렬 결과가 여러 개인 경우
+elif not certain:
+    print("?")
+# 위상 정렬을 수행한 결과 출력
+else:
+    for i in result:
+        print(i, end=' ')
+    print()
 ```
